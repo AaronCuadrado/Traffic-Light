68d313f9bc699bc89dfa32bfd226973347d9ce86
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-08-05 17:41:21 +0000

    finalizadogit add .git add .git add .

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 const Home = () => {
 	const [color, setColor] = useState('red');
 
+	const [showPurple, setShowPurple] = useState(false);
 
 	const cambioDeColor = () => {
 		if (color === 'red') {
@@ -11,6 +12,12 @@ const Home = () => {
 		} else if (color === 'yellow') {
 			setColor('green');
 		} else if (color === 'green') {
+			if (showPurple) {
+				setColor ('purple');
+			} else {
+				setColor ('red');
+			}
+		} else if (color === 'purple') {
 			setColor('red');
 		}
 	};
@@ -21,10 +28,14 @@ const Home = () => {
 				<button className={color === 'red' ? 'red light rojo' : 'red light'} onClick={() => setColor('red')}></button>
 				<button className={color === 'yellow' ? 'yellow light amarillo' : 'yellow light'} onClick={() => setColor('yellow')}></button>
 				<button className={color === 'green' ? 'green light verde' : 'green light'} onClick={() => setColor('green')}></button>
-				<button className={color === 'purple' ? 'purple light morado' : 'purple light'} onClick={() => setColor('purple')}>{color !== 'purple' && 'Morado'}</button>
+				{showPurple && (
+                    <button className={color === 'purple' ? 'purple light moradito' : 'purple light'} onClick={() => setColor('purple')}></button>
+                )}
 				
 			</div>
 			<button className="pulsador" onClick={cambioDeColor}>Cambiar color</button>
+			<button className="pulsador" onClick={() => setShowPurple(true)}>Añadir 4º color</button>
+			<button className="pulsador" onClick={() => setShowPurple(false)}>Quitar 4º color</button>
 		</>
 	);
 };
